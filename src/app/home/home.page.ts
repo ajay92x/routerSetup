284cc182680183c3b49606dfcfb3638ecb9a1cb8
ajay92x <ajay.verma1953@gmail.com>
2020-10-31 20:28:06 +0530

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public url: any = 'https://newmywifiext.com/';
+  public isConnect: boolean = false;
+
+  constructor(private domSanitizer: DomSanitizer) {
+    this.url = this.domSanitizer.bypassSecurityTrustResourceUrl(this.url);
+    // console.warn(this.url);
+  }
+
+  onStartConnecting(){
+    /* const browser = this.iab.create('https://newmywifiext.com/');
+    browser.show(); */
+    // browser.close()
+    // window.open(this.url, '_self');
+    this.isConnect = true;
+  }
+
+  backToMainPage(){
+    this.isConnect = false;
+  }
 
 }
